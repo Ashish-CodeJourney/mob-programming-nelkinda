6a3e92e2165aa9d5bb004625af6b0479d7f7c0c5
milan.vlaski <milan.vlaski@nelkinda.com>
2024-10-01 13:02:11 +0200

    doc: talked about the problem

--- README.md
+++ README.md
@@ -53,3 +53,16 @@ Output: 4
 Input: "MMDLXXXIII"
 Output: 2583
 ```
+
+# Notes
+Input: "MMDLXXXIII"
+Output: 2583
+
+III = 3
+LXXX = 50 + 10 + 10 +10
+MMD = 1000 + 1000 + 500
+
+
+I, (II, III)
+V (IV, V, VI, VII, VIII)
+IX
--- app/src/main/kotlin/mob/programming/nelkinda/RomanNumeral.kt
+++ app/src/main/kotlin/mob/programming/nelkinda/RomanNumeral.kt
@@ -1,16 +1,19 @@
 package mob.programming.nelkinda
 
 class RomanNumeral(val s: String) {
+
     fun toInt(): Int {
+
+
         if (s == "IV") {
             return 4
         }
+
         if (s == "V") {
             return 5
         }
 
-        val count = s.count()
-        return count
+        return s.count()
     }
 
 }
