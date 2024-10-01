3aba19a665d98a7af1f15c184a0f3beb14626589
vinayak <vinayak.t@vervetronics.com>
2024-10-01 15:48:44 +0530

    Refactor: Counts number of characters, returns it.

--- app/src/main/kotlin/mob/programming/nelkinda/RomanNumeral.kt
+++ app/src/main/kotlin/mob/programming/nelkinda/RomanNumeral.kt
@@ -2,13 +2,9 @@ package mob.programming.nelkinda
 
 class RomanNumeral(val s: String) {
     fun toInt(): Int {
-        if (s == "I") {
-            return 1
-        } else if (s == "II") {
-            return 2
-        } else {
-            return 3
-        }
+        val count = s.count()
+        return count
+
     }
 
 }
